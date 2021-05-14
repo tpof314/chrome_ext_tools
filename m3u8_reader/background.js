e02fa6a70ae43b439107d420e20b43b0bf8fc90a
Haojie Huang <hhuang3@woolworths.com.au>
2021-05-14 23:45:26 +1000

    add code

---
+++ m3u8_reader/background.js
@@ -0,0 +1,54 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+var global_m3u8_link = "";
+
+function is_m3u8_link(url) {
+  if (url.indexOf('.m3u8') > 5) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function print_info(tab) {
+  console.log(global_m3u8_link);
+  console.log(tab.title);
+}
+
+function pass_to_server(url, title) {
+  var payload = {
+      "url": url,
+      "title": title
+  };
+  
+  fetch('http://localhost:5000/save_m3u8_link', {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json, application/xml, text/plain, text/html, *.*',
+          'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'
+      },
+      body: new URLSearchParams(payload)
+  }).then(resp => console.log(resp));
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function(info) {
+    if (is_m3u8_link(info.url)) {
+      global_m3u8_link = info.url;
+    }
+    // return {requestHeaders: details.requestHeaders};
+  },
+  // filters
+  {urls: ['https://*/*', 'http://*/*']},
+  // extraInfoSpec
+  []);
+
+chrome.browserAction.onClicked.addListener((tab) => {
+  print_info(tab);
+  pass_to_server(global_m3u8_link, tab.title)
+});
---
+++ m4a_downloader/background.js
@@ -0,0 +1,49 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+var global_m4a_link = "";
+
+function is_m4a_link(url) {
+  if (url.indexOf('.m4a') > 5 || url.indexOf('.mp3') > 5) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function print_info(tab) {
+  console.log(global_m4a_link);
+  console.log(tab.title);
+}
+
+function download_file(url, title) {
+  chrome.downloads.download({
+    url: url, 
+    filename: title + ".m4a"
+  })
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+  function(info) {
+    if (is_m4a_link(info.url)) {
+      global_m4a_link = info.url;
+    }
+    // return {requestHeaders: details.requestHeaders};
+  },
+  // filters
+  {urls: ['https://*/*', 'http://*/*']},
+  // extraInfoSpec
+  []);
+
+chrome.browserAction.onClicked.addListener((tab) => {
+  print_info(tab);
+  download_file(global_m4a_link, tab.title);
+  // chrome.downloads.download({
+  //   url: global_m4a_link, 
+  //   filename: "1.m4a"
+  // })
+});
