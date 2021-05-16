d2bacf00bb5055e148c253613e73b531bfeacfc9
Haojie Huang <hhuang3@woolworths.com.au>
2021-05-17 08:31:35 +1000

    remove extra comments

--- m4a_downloader/background.js
+++ m4a_downloader/background.js
@@ -5,34 +5,35 @@
 'use strict';
 
 var global_m4a_link = "";
-// https://res.wx.qq.com/voice/getvoice?mediaid=MzU3MTcxNjI4OV8yMjQ3NDg4MDI1
-function is_m4a_link(url) {
-  if (url.indexOf('.m4a') > 5 || url.indexOf('.mp3') > 5) {
-    return true;
-  }
-  else {
-    return false;
-  }
-}
 
 function print_info(tab) {
   console.log(global_m4a_link);
   console.log(tab.title);
 }
 
+function update_title(title) {
+  var result = title.replace("正在播放:", "");
+  result = result.replace(":", "_");
+  result = result.replace("?", "_");
+  result = result.replace(" ", "_");
+  result = result.replace("(", "_");
+  result = result.replace(")", "_");
+  result = result.replace("|", "_");
+  result = result.replace("-", "_");
+  result = result.substr(0, 30);
+  return result;
+}
+
 function download_file(url, title) {
   chrome.downloads.download({
     url: url, 
-    filename: title + ".m4a"
+    filename: update_title(title) + ".m4a"
   })
 }
 
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {
-    if (is_m4a_link(info.url)) {
-      global_m4a_link = info.url;
-    }
-    else if (info.type == "media") {
+    if (info.type == "media") {
       global_m4a_link = info.url;
     }
     // return {requestHeaders: details.requestHeaders};
